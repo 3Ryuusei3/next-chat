ccbeab1a6388d019d9e49c5b8c959d95e2eb51f3
Manuel Atance <atance3@hotmail.com>
2024-07-03 19:19:19 +0200

    Add zustand and create user state

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import ChatHeader from '@/components/ChatHeader'
 import { supabaseServer } from '@/lib/supabase/server'
+import InitUser from '@/lib/store/initUser';
 
 export default async function Page() {
 
@@ -9,10 +10,13 @@ export default async function Page() {
   const { data } = await supabase.auth.getUser() ?? { data: null };
 
   return (
-    <div className='max-w-3xl mx-auto md:my-10 h-screen'>
-      <div className='h-full border rounded-md'>
-        <ChatHeader user={data?.user} />
+    <>
+      <div className='max-w-3xl mx-auto md:my-10 h-screen'>
+        <div className='h-full border rounded-md'>
+          <ChatHeader user={data?.user} />
+        </div>
       </div>
-    </div>
+      <InitUser user={data?.user}  />
+    </>
   )
 }
